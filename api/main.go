9aef98963d72c4317c833a0735a34db30f85d3e2
lczm <zemingchua@gmail.com>
2025-02-14 16:14:52 +0800

    chore: add my own domain for deployment

--- api/main.go
+++ api/main.go
@@ -183,7 +183,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedOrigins: []string{"http://localhost:3000", "https://lczm.me"},
 		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
 		AllowedHeaders: []string{"Content-Type"},
 	}))
